8ee03cbe56f6ae43e805e6134f6af6b797690584
puag <puag@163.com>
2014-05-14 18:17:06 +0800

    test

--- js/jing.js
+++ js/jing.js
@@ -1195,6 +1195,7 @@
         }
     });
 
+    // TODO 暂时的替代方法
     $.fn.show = function(){
         this.css({ display : 'block' });
     }
@@ -1211,6 +1212,156 @@
     });
     */
 
+    var optionsCache = {};
+    _.createOptions = function(options){
+        var object = optionsCache[options] = {};
+        $.each(options.match(rnotwhite) || [], function(i, flag){
+            object[flag] = true;
+        });
+        return object;
+    }
+
+    // 回调函数
+    $.Callbacks = function(options){
+        options = typeof options === 'string' ?
+            (optionsCache[options] || _.createOptions(options)) :
+            $.extend({}, options);
+
+        var firing,
+            memory,
+            fired,
+            firingStart,
+            firingLength,
+            firingIndex,
+            list = [], // 回调列表
+            stack = !options.once && [],
+            fire = function(data){
+                memory = options.memory && data; // 如果参数memory为true，则记录data
+                fired = true; // 标记触发回调
+                firingIndex = firingStart || 0;
+                firingStart = 0;
+                firingLength = list.length;
+                firing = true; // 标记正在触发回调
+
+                for( ; list && firingIndex < firingLength; firingIndex++){
+                    if(list[firingIndex].apply(data[0], data[1]) === false && options.stopOnFalse){
+                        memory = false; // 阻止未来可能由于add所产生的回调
+                        break; // 由于参数stopOnFalse为true，所以当有回调函数返回值为false时退出循环
+                    }
+                }
+                firing = false; // 标记回调结束
+                if(list){
+                    if(stack){
+                        if(stack.length){
+                            fire(stack.shift());
+                        }
+                    } else if(memory){ // 否则，如果有记忆
+                        list = [];
+                    } else { // 再否则阻止回调列表中的回调
+                        self.disable();
+                    }
+                }
+            },
+            self = {
+                // 添加回调列表
+                add : function(){
+                    if(list){
+                        var start = list.length;
+
+                        (function add(args){
+                            $.each(args, function(i, arg){
+                                var type = $.type(args);
+                                
+                                if(type === 'function'){
+                                    if(!options.unique || !self.has(arg)){
+                                        list.push(arg);
+                                    }
+                                } else if(arg && arg.length && type !== 'string'){ // 假如传过来的参数为数组或array-like，则继续调用添加，从这里可以看出add的传参可以有add(fn),add([fn1,fn2]),add(fn1,fn2)
+                                    add(arg);
+                                }
+                            });
+                            
+                            if(firing){
+                                firingLength = list.length;
+                            } else if(memory){
+                                firingStart = start;
+                                fire(memory);
+                            }
+                        }(arguments));
+                    }
+                    return this;
+                },
+                remove : function(){
+                    if(list){
+                        $.each(arguments, function(i, arg){
+                            var index;
+                            while((index = $.inArray(arg, list, index)) > -1){
+                                list.splice(index, 1);
+                                if(firing){
+                                    if(index < firingLength){
+                                        firingLength--;
+                                    }
+                                    if(index <= firingIndex){
+                                        firingIndex--;
+                                    }
+                                }
+                            }
+                        });
+                    }
+                    return this;
+                },
+                has : function(fn){
+                    return fn ? $.inArray(fn, list) > -1 : !!(list && list.length);
+                },
+                empty : function(){
+                    list = [];
+                    firingLength = 0;
+                    return this;
+                },
+                disable : function(){
+                    list = stack = memory = undefined;
+                    return this;
+                },
+                disabled : function(){
+                    return !list;
+                },
+                lock : function(){
+                    stack = undefined;
+                    if(!memory){
+                        self.disabled();
+                    }
+                    return this;
+                },
+                locked : function(){
+                    return !stack;
+                },
+                fireWith : function(context, args){
+                    if(list && (!fired || stack)){
+                        args = args || [];
+                        args = [context, args.slice ? args.slice() : args];
+                        
+                        if(firing){
+                            stack.push(args);
+                        } else {
+                            fire(args);
+                        }
+                    }
+                    return this;
+                },
+                fire : function(){
+                    self.fireWith(this, arguments);
+                    return this;
+                },
+                fired : function(){
+                    return !!fired;
+                }
+            };
+
+        return self;
+    }
+
+
+
 }(window));
 
 // 2014-04-21 : 准备开发第一版
@@ -1223,3 +1374,4 @@
 // 2014-05-09 : 增加$().width()方法, event first;
 // 2014-05-12 : 增加$().data(), $().removeData()方法
 // 2014-05-13 : 增加$().event.dispatch方法
+// 2014-05-14 : 增加$().CallBacks()方法
